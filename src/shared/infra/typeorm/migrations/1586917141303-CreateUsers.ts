6942cb108fd3feb08fd2cf6a402c40d09179a9b1
Jr Bytes <itamazor@gmail.com>
2020-08-22 18:29:55 -0300

    Initial commit. Configuring user table.

---
+++ src/shared/infra/typeorm/migrations/1586917141303-CreateUsers.ts
@@ -0,0 +1,67 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export default class CreateUsers1586917141303 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'default_minute',
+            type: 'integer',
+            default: "'25'",
+          },
+          {
+            name: 'setting_progress_bar',
+            type: 'boolean',
+            default: "'true'",
+          },
+          {
+            name: 'theme',
+            type: 'varchar',
+            default: "'light'",
+          },
+          {
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+}
