38e30bbec475c3de1ad4286a08dc806db838a288
Jr Bytes <itamazor@gmail.com>
2020-08-25 11:40:49 -0300

    Created tasks migration.

---
+++ src/shared/infra/typeorm/migrations/1598365682083-CreateTasks.ts
@@ -0,0 +1,64 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm'
+
+export class CreateTasks1598365682083 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tasks',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'realized_pomos',
+            type: 'integer',
+          },
+          {
+            name: 'project_id',
+            type: 'uuid',
+            default: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    )
+
+    await queryRunner.createForeignKey(
+      'projects',
+      new TableForeignKey({
+        name: 'TaskProject',
+        columnNames: ['project_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'projects',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tasks')
+  }
+}
